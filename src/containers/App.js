1976c3fa3cc7eb42328b3553b5da8956a6fcff16
John Haab <johnlhaab@gmail.com>
2023-01-31 10:58:34 -0500

    Making it more optimized.

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,12 +24,13 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    return (
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
       <div className="tc sans-serif">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
